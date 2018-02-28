8daa9fb49b0a396f4f381bdc38f9fdde70855fd4
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-02-28 16:51:09 +0800

    update

--- cloudformation/update_network_cloudformation.sh
+++ cloudformation/update_network_cloudformation.sh
@@ -8,5 +8,5 @@ fi
 cf_stackname=BasicVPC
 s3_bucket=${env}-kiwi-cloudformation-scripts
 
-aws cloudformation deploy --stack-name ${cf_stackname} --s3-bucket ${s3_bucket} --force-upload --template-file  ./update_network_cloudformation.json 
+aws cloudformation deploy --stack-name ${cf_stackname} --s3-bucket ${s3_bucket} --force-upload --template-file  ./vpc_basic.yaml
 
