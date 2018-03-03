f92b682586e514e47f72ff2b37be74e50a8f6225
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-03-03 16:45:37 +0800

    update

--- init/install_dashboard.sh
+++ init/install_dashboard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml~
+kubectl apply -f ./kubernetes-dashboard.yaml
